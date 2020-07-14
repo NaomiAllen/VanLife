01107cf71c16ce1af70c1bda63847e2befd58645
Naomi <naomi@macbook-pro.attlocal.net>
2020-07-13 22:49:46 -0500

    edit, submit buttons

--- models/blog.js
+++ models/blog.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const blogSchema = new mongoose.Schema({
     title:  { type: String, required: true },
     location: String,
-    body: String
+    body: String,
+    
     
 })
 
